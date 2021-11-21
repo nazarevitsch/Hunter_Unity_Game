2b72bed1c355e3d89768f775dedb0eb1c8d21194
nazarevitsch <bidaritter1997@gmail.com>
2021-11-21 22:08:51 +0200

    map was added

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using View;
+
+namespace Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private PlayerView _playerView;
+        void Awake()
+        {
+            _playerView = FindObjectOfType<PlayerView>();
+        }
+
+        void Update()
+        {
+            _playerView.movement.x = Input.GetAxisRaw("Horizontal");
+            _playerView.movement.y = Input.GetAxisRaw("Vertical");
+            _playerView.Move();
+        }
+    }
+}
---
+++ Assets/Scripts/View/PlayerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace View
+{
+    public class PlayerView : MonoBehaviour
+    {
+        public Rigidbody2D rb;
+        public float movementSpeed = 4;
+        public Vector2 movement;
+        
+        public void Move()
+        {
+            rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+}
