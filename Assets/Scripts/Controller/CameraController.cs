99be15095c8d487276c689fe92afb01160e99636
nazarevitsch <bidaritter1997@gmail.com>
2021-11-25 20:47:16 +0200

    now camera follow player

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controller
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        
+        public float smoothMove = 0.125f;
+        
+        
+        void LateUpdate()
+        {
+            Vector3 newPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPosition, smoothMove);
+        }
+    }
+}
